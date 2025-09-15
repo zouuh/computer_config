c707178402f1bac660ce94464acf461b2ed6c79e
zouuh <45451432+zouuh@users.noreply.github.com>
2025-09-15 20:18:47 +0200

    feat: add theme terafox

---
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -0,0 +1,15 @@
+return {
+  -- add themes
+  { "savq/melange-nvim" },
+  { "vague2k/vague.nvim" },
+  { "EdenEast/nightfox.nvim" },
+  { "rebelot/kanagawa.nvim" },
+
+  -- Configure LazyVim to load gruvbox
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "terafox",
+    },
+  },
+}
